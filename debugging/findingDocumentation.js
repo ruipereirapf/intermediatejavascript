2d8053f3d703c37164cb741ca94291d7bca2a4da
Rui Pereira <ruipereira.pf@gmail.com>
2024-05-21 15:06:49 +0100

    debugging and errorhandling

---
+++ debugging/findingDocumentation.js
@@ -0,0 +1,20 @@
+/*
+
+// Link to String.repeat() documentation: 
+function doubleString(string) {
+  return string.repeat();
+}
+
+// Should return 'echoecho'
+console.log("doubleString('echo') returns: " + doubleString('echo'));
+
+*/
+
+
+// Link to String.repeat() documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+function doubleString(string) {
+    return string.repeat(2);
+  }
+  
+  // Should return 'echoecho'
+  console.log("doubleString('echo') returns: " + doubleString('echo'));
